b666742a9c473da18f59bff9f4556f1dce361349
Benjamin Fischer <b.fis@cern.ch>
2018-05-04 11:14:18 +0200

    Add utility functions.

--- numphy/util.py
+++ numphy/util.py
@@ -36,6 +36,35 @@ def expand_ellipsis(values, size):
     idx = values.index(Ellipsis)
     return values[:idx] + n * (empty_slice,) + values[idx + 1:]
 
+def ellipsis_eqiv(key):
+    """
+    Returns whether *key* is a *tuple* of *Ellipsis* or equivalent *slice*s.
+    """
+    return isinstance(key, tuple) and all(
+        k is Ellipsis or
+        isinstance(k, slice) and (
+            k.start in (None, 0) and
+            k.stop in (None, -1) and
+            k.step in (None, 1)
+        )
+        for k in key
+    )
+
+def slice_len(sli, ref=None):
+    """
+    Returns the expected length of the slice *sli*, possibly consulting the reference length *ref*
+    or *len(ref)* if the former is not an *int*. Will return *None* if a *ref* is needed but *None*.
+    """
+    b, e, s = sli.start, sli.stop, sli.step
+    if s is None:
+        s = 1
+    if (0 <= b and 0 <= e) or (b < 0 and e < 0):
+        return len(xrange(b, e, abs(s)))
+    elif ref is not None:
+        if not isinstance(ref, int):
+            ref = len(ref)
+        return len(xrange(*sli.indices(ref)))
+
 
 class NoValue(object):
 
